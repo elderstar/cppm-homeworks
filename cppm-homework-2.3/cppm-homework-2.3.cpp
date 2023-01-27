a549648c9a592dd21fc4fb5d06a2c5765b4cd57f
Сергей Старовойтов <starovoytovsergeya@gmail.com>
2023-01-27 19:22:56 +0300

    add cppm-homework-2.3

---
+++ cppm-homework-2.3/cppm-homework-2.3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+struct Address {
+    std::string     country;
+    std::string     city;
+    std::string     street;
+    int             building;
+    int             apt;
+    int             postal_code;
+};
+
+void printStruct(const Address& address) {
+
+    std::cout << "Страна: " << address.country << "\n";
+    std::cout << "Город: " << address.city << "\n";
+    std::cout << "Улица: " << address.street << "\n";
+    std::cout << "Номер дома: " << address.building << "\n";
+    std::cout << "Номер квартиры: " << address.apt << "\n";
+    std::cout << "Индекс: " << address.postal_code << "\n";
+    std::cout << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int count = 2;
+
+    Address struct_arr[count] = {
+        { "Россия", "Москва", "Арбат", 12, 8, 123456 },
+        { "Россия", "Ижевск", "Пушкина", 59, 143, 953769 }
+    };
+
+    for (int i = 0; i < count; ++i) {
+
+        printStruct(struct_arr[i]);
+    }
+}
