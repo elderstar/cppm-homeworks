3e8c35b6814f3760eaf37a195739da4ddb9f9864
Сергей Старовойтов <starovoytovsergeya@gmail.com>
2023-01-30 19:50:19 +0300

    add homework 3.1 & 3.2

---
+++ cppm-homework-3.1/cppm-homework-3.1.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+
+class Calculator
+{
+public:
+
+    double add(double num1, double num2)
+    {
+        return num1 + num2; 
+    }
+
+    double multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    double subtract_1_2(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    double subtract_2_1(double num1, double num2)
+    {
+        return num2 - num1;
+    }
+
+    double divide_1_2(double num1, double num2)
+    {
+        return num1 / num2;
+    }
+
+    double divide_2_1(double num1, double num2)
+    {
+        return num2 / num1;
+    }
+
+    bool set_num1(double num1)
+    {
+        if (num1 == 0) {
+            std::cout << "Неверный ввод!\n";
+            return false;
+        }
+        else {
+            this->num1 = num1;
+            return true;
+        }
+    }
+
+    bool set_num2(double num2)
+    {
+        if (num2 == 0) {
+            std::cout << "Неверный ввод!\n";
+            return false;
+        }
+        else {
+
+            this->num2 = num2;
+            return true;
+        }
+    }
+
+    ~Calculator() {};
+
+private:
+    double num1, num2;
+};
+
+void printCalculations(Calculator& calc, double num1, double num2) {
+
+    std::cout << "num1 + num2 = " << calc.add(num1, num2) << "\n";
+    std::cout << "num1 - num2 = " << calc.subtract_1_2(num1, num2) << "\n";
+    std::cout << "num2 - num1 = " << calc.subtract_2_1(num1, num2) << "\n";
+    std::cout << "num1 * num2 = " << calc.multiply(num1, num2) << "\n";
+    std::cout << "num1 / num2 = " << calc.divide_1_2(num1, num2) << "\n";
+    std::cout << "num2 / num1 = " << calc.divide_2_1(num1, num2) << "\n";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double num1 = 0;
+    double num2 = 0;
+
+    Calculator calc{};
+
+    do {
+        do {
+            std::cout << "Введите num1 : ";
+            std::cin >> num1;
+        } while (!calc.set_num1(num1));
+
+        do {
+            std::cout << "Введите num2 : ";
+            std::cin >> num2;
+        } while (!calc.set_num2(num2));
+
+        printCalculations(calc, num1, num2);
+
+    } while (true);
+
+    return 0;
+}
---
+++ cppm-homework-3.2/cppm-homework-3.2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+class Counter 
+{
+public:
+
+    Counter(int value) {
+        start_value = value;
+    };
+    Counter() {
+        start_value = 0;
+    };
+    ~Counter() {};
+
+    void subtract() {
+    
+        std::cout << --this->start_value << "\n";
+    }
+
+    void add() {
+
+        std::cout << ++this->start_value << "\n";
+    }
+
+    void printVal() {
+
+        std::cout << this->start_value << "\n";
+    }
+private:
+
+    int start_value;
+};
+
+void count(Counter& counter) 
+{
+    char command = '0';
+
+    do {
+
+        std::cout << "Введите команду('+', '-', '=' или 'x') : ";
+        std::cin >> command;
+        switch (command) {
+        case '+':
+            counter.add();
+            break;
+        case '-':
+            counter.subtract();
+            break;
+        case '=':
+            counter.printVal();
+            break;
+        }
+    } while (command != 'x');
+}
+
+int main()
+{
+    system("chcp 1251");
+    std::string answer;
+
+    std::cout << "Вы хотите указать начальное значение счётчика ? Введите да или нет : ";
+    std::cin >> answer;
+
+    if (answer == "да") {
+        int start_value = 0;
+        std::cout << "Введите начальное значение счётчика : ";
+        std::cin >> start_value;
+        Counter counter(start_value);
+        count(counter);
+    }
+    else {
+        Counter counter;
+        count(counter);
+    }
+    
+    std::cout << "До свидания!";
+    return 0;
+}
